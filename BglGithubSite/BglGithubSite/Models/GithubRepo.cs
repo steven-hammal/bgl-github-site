24fe9b41d26530708a6517379ff826cd815201b8
Steven Hammal <hammalsj@gmail.com>
2019-10-13 10:28:21 +0100

    Avatar now displays in search result view, top 5 repos names now act as links to repositories

--- BglGithubSite/BglGithubSite/Models/GithubRepo.cs
+++ BglGithubSite/BglGithubSite/Models/GithubRepo.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace BglGithubSite.Models
+﻿namespace BglGithubSite.Models
 {
     public class GithubRepo : BaseCodeRepo
     {
